27923d5bfba9259454b5c6efbbda3d88f24c8b97
Quinn White <qwhite@smith.edu>
2023-04-13 09:25:07 -0400

    update all

--- thesis/generate_citations.R
+++ thesis/generate_citations.R
@@ -1,4 +1,12 @@
-rbbt::bbt_write_bib('./thesis/bibliography.json',
+# rbbt::bbt_write_bib('./thesis/bibliography.json',
+#                     keys = rbbt::bbt_detect_citations(
+#                       list.files("./thesis/", 
+#                                  pattern ='*.Rmd',
+#                                  full.names=TRUE)),
+#                     overwrite = TRUE)
+
+
+rbbt::bbt_write_bib('./thesis/bibliography.bib',
                     keys = rbbt::bbt_detect_citations(
                       list.files("./thesis/", 
                                  pattern ='*.Rmd',
